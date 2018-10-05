859e326de0e5c89cd87ad2ac1f145b3b5af71492
wangme <wangme@lafayette.edu>
2018-10-05 01:23:30 -0400

    psycopg

--- smallgreenblog/settings.py
+++ smallgreenblog/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'smallgreenblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'smallgreenblog',
         'USER': 'janus',
         'PASSWORD': '',
