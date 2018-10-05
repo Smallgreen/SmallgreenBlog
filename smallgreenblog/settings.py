a8d11f1ba568cf0f286a0679ec6213ecf9f1ccea
wangme <wangme@lafayette.edu>
2018-10-05 16:07:22 -0400

    addp

--- smallgreenblog/settings.py
+++ smallgreenblog/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'smallgreenblog',
         'USER': 'janus',
-        'PASSWORD': '',
+        'PASSWORD': 'coke',
         'HOST': '127.0.0.1',
         'PORT': '',
     }
