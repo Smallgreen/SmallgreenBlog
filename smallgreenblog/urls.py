fc29842637365c0fd37a8fd70248f74e82ddea24
wangme <wangme@lafayette.edu>
2018-10-05 23:08:26 -0400

    fix import

--- smallgreenblog/urls.py
+++ smallgreenblog/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.core.urlsresolvers import path
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url('admin/', admin.site.urls),
 ]
